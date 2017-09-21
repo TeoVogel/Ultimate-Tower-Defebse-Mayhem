b7e564727a415c6e20f4aaec5a7c39d47ca5abb5
Cula97 <31107333+Cula97@users.noreply.github.com>
2017-09-21 02:02:34 -0300

    algo

--- Eclipse/Project/src/loquequieras/Ente.java
+++ Eclipse/Project/src/loquequieras/Ente.java
@@ -4,7 +4,14 @@ public abstract class Ente {
 	protected int vida;
 	protected JLabel grafico;
 	protected Icon[] imagen;
+	protected Celda celda;
 	
+	public int getVida(){
+		return vida;
+	}
 	
+	public Celda getCelda() {
+		return celda;
+	}
 
 }
