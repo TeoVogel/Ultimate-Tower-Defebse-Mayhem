9edec60921ba5d3a5cfedd468d8348bc4afaf69d
guidox98 <guidox98@gmail.com>
2017-10-15 19:16:00 -0300

    primera

---
+++ Eclipse/Project/src/grafica/GraficoDisparo.java
@@ -0,0 +1,13 @@
+package grafica;
+import java.awt.Point;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+public class GraficoDisparo{
+
+	public static void crearDisparo(Point a, Point b, String dir){
+		Point ini=new Point((int)a.getX()+ Grafico.width ,(int)a.getY()+ Grafico.height/2);
+		
+		
+	}
+}
