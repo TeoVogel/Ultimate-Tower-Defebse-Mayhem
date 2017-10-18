98b7f67dde4fedfa915c8b8ea01f325325e9008c
TeoVogel <teovogel97@gmail.com>
2017-10-18 14:31:35 -0300

    Enemigo Factory update

--- Eclipse/Project/src/juego/EnemigoFactory.java
+++ Eclipse/Project/src/juego/EnemigoFactory.java
@@ -4,12 +4,11 @@ import java.util.Random;
 
 import juego.ente.Celda;
 import juego.ente.Enemigo;
-import juego.ente.Enemigo1;
 
 public class EnemigoFactory {
 
 	public static Enemigo crearEnemigo (Celda celda) {
-		return new Enemigo1(celda);
+		return new Enemigo(1,1,1,1,20);
 	}
 	
 }
