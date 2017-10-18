2a8b695b6a7053ebcaa34ed6d8033b0718685c22
TeoVogel <teovogel97@gmail.com>
2017-10-18 19:30:20 -0300

    Fondo

--- Eclipse/Project/src/grafica/Interfaz.java
+++ Eclipse/Project/src/grafica/Interfaz.java
@@ -7,25 +7,36 @@ import java.awt.event.MouseEvent;
 
 import juego.ente.Ente;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.text.IconView;
 
 import juego.Juego;
 import juego.acciones.AccionSpawnearEnemigo;
 
 public class Interfaz extends JFrame{
-	private JPanel contentPane;
+	private JLabel contentPane;
 	private Juego juego;
+
+//	protected final static String path = "C:/Users/Franco/Documents/GitHub/Ultimate-Tower-Defense-Mayhem/Eclipse/Project/src/assets/"; 
+	protected final static String path = "C:/Users/teo/Documents/TDP/Ultimate-Tower-Defense-Mayhem/Eclipse/Project/src/assets/"; 
+//	protected final static String path = "C:/Users/guido/Documents/GitHub/Ultimate-Tower-Defense-Mayhem/Eclipse/Project/src/assets/"; 
+
 	
 	public Interfaz(Juego j) {
 		juego=j;
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 1000, 600);
-		contentPane = new JPanel();
+		setBounds(0, 0, 1000, 638); // TODO: fix, 600 + 38 es por la tittle bar de windows
+		contentPane = new JLabel();
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		contentPane.setIcon(new ImageIcon(path + "fondo.png"));
+		
 		setVisible(true);
 		
 		addMouseListener(new MouseAdapter() {
