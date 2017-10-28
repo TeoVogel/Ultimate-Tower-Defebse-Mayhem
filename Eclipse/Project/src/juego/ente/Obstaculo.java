5d509104db0d1e4912cd85b45e8bd22784ab67d1
TeoVogel <teovogel97@gmail.com>
2017-10-28 18:43:38 -0300

    Creando obstaculo

--- Eclipse/Project/src/juego/ente/Obstaculo.java
+++ Eclipse/Project/src/juego/ente/Obstaculo.java
@@ -1,15 +1,22 @@
 package juego.ente;
 
 import grafica.GraficoEnte;
+import juego.visitor.Visitor;
 
 /*
  * TODO: algunos obstaculos tienen que ser comprables
  */
 
-public abstract class Obstaculo extends Ente {
+public class Obstaculo extends Ente {
 	
 	public Obstaculo(int vida) {
 		super(vida);
+		grafica = new GraficoEnte(this, "a3");
+	}
+
+	@Override
+	public void accept(Visitor v) {
+		// TODO Auto-generated method stub		
 	}
 	
 }
--- Eclipse/Project/src/juego/niveles/Nivel1.java
+++ Eclipse/Project/src/juego/niveles/Nivel1.java
@@ -1,5 +1,10 @@
 package juego.niveles;
 
+import juego.Juego;
+import juego.Mapa;
+import juego.ente.Celda;
+import juego.ente.Obstaculo;
+
 public class Nivel1 extends Nivel {
 
 	public Nivel1 (int d) {
@@ -8,6 +13,15 @@ public class Nivel1 extends Nivel {
 		/*
 		 * TODO: crear obstaculos y setear el fondo del mapa
 		 */
+		
+		Obstaculo o1 = new Obstaculo(100);
+		Juego juego = Juego.getJuego();
+		Mapa mapa = juego.getMapa();
+		Celda celda = mapa.getCelda(1, 1);
+		//o1.init(celda);
+
+		Obstaculo o2 = new Obstaculo(100);
+		//o2.init(Juego.getJuego().getMapa().getCelda(3, 6));
 		
 	}
 	
