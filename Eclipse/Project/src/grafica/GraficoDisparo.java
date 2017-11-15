97883356cebc48dfc83553b592e8c2ebf0500218
guidox98 <guidox98@gmail.com>
2017-11-15 16:28:06 -0300

    disparo correcto

--- Eclipse/Project/src/grafica/GraficoDisparo.java
+++ Eclipse/Project/src/grafica/GraficoDisparo.java
@@ -1,15 +1,47 @@
 package grafica;
 
+import java.awt.Image;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import juego.Constantes;
 import juego.Juego;
 
 public abstract class GraficoDisparo {
 	protected int altoDisparo = 10;
+	protected int corrimiento;
 	
-	protected PanelMapa panelMapa;
+	protected ImageIcon disparo;
+	protected JLabel grafico;
 	
 	public GraficoDisparo(){
-		//panelMapa = Juego.getJuego().getInterfaz().getPanelMapa();
 	}
 	
-	public abstract void graficar(GraficoEnte ataca, GraficoEnte recibe);
+	public void graficar(GraficoEnte ataca, GraficoEnte recibe){
+		int largo;
+		
+		int X = ataca.getX() + Constantes.width; 										// la derecha del atacante
+		int Y = ataca.getY() + Constantes.height/2 - altoDisparo/2 + corrimiento;		//y = centra el disparo en el medio del atacante
+		Point izq = new Point( X , Y);													// [a]- izq es donde el guion toca al corchete (a= atacar)
+		
+		X = recibe.getX();									 							//la izquierda del que recibe
+		Y = recibe.getY()+ Constantes.height/2 - altoDisparo/2 + corrimiento;			// centra el disparo en el medio del qe recibe	
+		Point der = new Point( X , Y );						  							// -[r] der es donde el guion toca al corchete (r= recibir)
+		
+		largo=(int) (der.getX()-izq.getX() + 1);// largo del disparo= distancia desde izq hasta der
+		
+		Image imagenAlargada = disparo.getImage(); 
+		imagenAlargada = imagenAlargada.getScaledInstance( largo, altoDisparo,  java.awt.Image.SCALE_DEFAULT);
+		
+		disparo = new ImageIcon(imagenAlargada);
+		grafico = new JLabel(disparo);
+		
+		grafico.setBounds( (int)izq.getX(), (int)izq.getY(), largo, altoDisparo );
+		grafico.setVisible(true);
+		Juego.getJuego().getInterfaz().getPanelMapa().add(grafico);
+		Juego.getJuego().getInterfaz().getPanelMapa().graficoTemporal(grafico, 5);
+		grafico.repaint();
+	}
 }
