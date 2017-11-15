8d670b1fcf2a2f180bbec089eeb2bf319405a33c
guidox98 <guidox98@gmail.com>
2017-11-15 16:31:34 -0300

    RayoLaserEnemigo

--- Eclipse/Project/src/grafica/RayoLaserAliado.java
+++ Eclipse/Project/src/grafica/RayoLaserAliado.java
@@ -5,11 +5,9 @@ import juego.Constantes;
 
 public class RayoLaserAliado extends GraficoDisparo {
 	
-	
 	public RayoLaserAliado(){
 		super();
 		disparo = new ImageIcon(Constantes.path+"atacks/disp_aliado.png");
 		corrimiento = 10;
-	}
-	
+	}	
 }
